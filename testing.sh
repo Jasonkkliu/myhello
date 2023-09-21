baa5ce5653f64d5417dfd9e228639d7e8d1e11d6
Jasonkkliu <b00502097@gmail.com>
2023-09-21 21:15:23 +0800

    init commit

---
+++ testing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Program:
+#       This program shows "this is a test from Zong-Sheng Liu" in your screen.
+# History:
+# 2023/09/16	Jason	First release
+
+echo "this is a test from Zong-Sheng Liu" >&2
+exit 0
